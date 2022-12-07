3a51ebb7aceb37628146918acf10e68ec3b593a2
GabryellPontes <lucaas.gabryell17@gmail.com>
2022-12-07 11:58:57 -0300

    inclusão de api e formularios hook

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,10 @@
-import { MdEmail, MdLock } from 'react-icons/md'
 import { useNavigate } from 'react-router-dom';
+import { MdEmail, MdLock } from 'react-icons/md'
+import { api } from "../../services/api";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+import {useForm} from 'react-hook-form';
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
@@ -7,41 +12,63 @@ import { Input } from "../../components/Input";
 
 import { Column, Container, CriarText, EsqueciText, Row, SubTitleLogin, Title, TitleLogin, Wrapper} from './styles';
 
-const Login = () => {
+const schema = yup.object({
+	email: yup.string().email('email não é valido').required('Campo obrigatorio'),
+	password: yup.string().min(3, 'No minimo 3 caracteres').required('Campo obrigatorio'),
+  }).required();
 
+
+const Login = () => {
 	const navigate = useNavigate();
 
-	const handleClickSignIn = () => {
-        navigate('/feed')
-    }
+	const { control, handleSubmit, formState: { errors } } = useForm({
+		resolver: yupResolver(schema),
+		mode: 'onChange',
+	});
+
+	const onSubmit = async (formData) => {
+        try{
+            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
+            
+            if(data.length && data[0].id){
+                navigate('/feed') 
+                return
+            }
+			
+            alert('Usuário ou senha inválido')
+        }catch(e){
+			//TODO: HOUVE UM ERRO
+        }
+    };
+	
+	console.log('errors', errors);
 
 	return (<>
-		<Header />
-		<Container>
-			<Column>
-			<Title>
-				A plataforma para você aprender com experts, dominar as princiapais tecnologias
-					e entrar mais rápido nas empresas mais desejadas.
-			</Title>
-			</Column>
-			<Column>
-			<Wrapper>
-				<TitleLogin>Faça seu cadastro</TitleLogin>
-				<SubTitleLogin>Faça seu login e make the chanpe</SubTitleLogin>
-				<form>
-					<Input placeholder="email" leftIcon={<MdEmail />}/>
-					<Input placeholder="senha" type="password" leftIcon={<MdLock />}/>
-					<Button title="Entrar" variant="secondary" onClick={handleClickSignIn} type="button" />
-				</form>
-				<Row>
-					<EsqueciText>Esqueci minha senha</EsqueciText>
-					<CriarText>Criar Conta</CriarText>
-				</Row>
-
-			</Wrapper>
-			</Column>
-		</Container>
-	</>)
+        <Header />
+        <Container>
+            <Column>
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
+                 e entrar mais rápido nas empresas mais desejadas.</Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                <TitleLogin>Faça seu cadastro</TitleLogin>
+                <SubTitleLogin>Faça seu login e make the change._</SubTitleLogin>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
+                    {errors.email && <span>E-mail é obrigatório</span>}
+                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
+                    {errors.senha && <span>Senha é obrigatório</span>}
+                    <Button title="Entrar" variant="secondary" type="submit"/>
+                </form>
+                <Row>
+                    <EsqueciText>Esqueci minha senha</EsqueciText>
+                    <CriarText>Criar Conta</CriarText>
+                </Row>
+                </Wrapper>
+            </Column>
+        </Container>
+    </>)
 }
 
 export { Login }
